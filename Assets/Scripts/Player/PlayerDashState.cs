9982c90fb9cac2258bab1df5e9b18349c6ae135d
Phuoc Dinh <phuocdinh462@gmail.com>
2023-10-29 22:17:53 +0700

    Improving skills in a skill tree

--- Assets/Scripts/Player/PlayerDashState.cs
+++ Assets/Scripts/Player/PlayerDashState.cs
@@ -10,7 +10,7 @@ public class PlayerDashState : PlayerState
   {
     base.Enter();
 
-    player.skill.clone.CreateClone(player.transform, Vector3.zero);
+    player.skill.clone.CreateCloneOnDashStart();
 
     stateTimer = player.dashDuration;
   }
@@ -18,6 +18,8 @@ public class PlayerDashState : PlayerState
   public override void Exit()
   {
     base.Exit();
+
+    player.skill.clone.CreateCloneOnDashOver();
     player.SetVelocity(0, rb.velocity.y);
   }
 
