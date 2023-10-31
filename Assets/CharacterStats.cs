ce264e669c651edf5ae56743e1a77f506543558f
Phuoc Dinh <phuocdinh462@gmail.com>
2023-10-31 18:42:18 +0700

    Critical attack

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CharacterStats : MonoBehaviour
 {
@@ -11,17 +12,22 @@ public class CharacterStats : MonoBehaviour
   public Stat intelligence; // 1 point increase magic damge by 1 and magic resistance by 3
   public Stat vitality; // 1 point increase health by 3 or 5 points
 
+  [Header("Offensive stats")]
+  public Stat damage;
+  public Stat critChance;
+  public Stat critPower; // Default value 150%
+
   [Header("Defensive stats")]
   public Stat maxHealth;
   public Stat armor;
   public Stat evasion;
 
-  public Stat damage;
 
   [SerializeField] private int currentHealth;
 
   protected virtual void Start()
   {
+    critPower.SetDefaultValue(150);
     currentHealth = maxHealth.GetValue();
   }
 
@@ -30,6 +36,9 @@ public class CharacterStats : MonoBehaviour
     if (TargetCanAvoidAttack(_targetStats)) return;
 
     int totalDamage = damage.GetValue() + strength.GetValue();
+
+    if (CanCrit())
+      totalDamage = CalculateCriticalDamage(totalDamage);
 
     totalDamage = CheckTargetArmor(_targetStats, totalDamage);
     _targetStats.TakeDamage(totalDamage);
@@ -63,5 +72,21 @@ public class CharacterStats : MonoBehaviour
       return true;
 
     return false;
+  }
+
+  private bool CanCrit()
+  {
+    int totalCriticalChance = critChance.GetValue() + agility.GetValue();
+
+    if (Random.Range(0, 100) <= totalCriticalChance) return true;
+
+    return false;
+  }
+
+  private int CalculateCriticalDamage(int _damage)
+  {
+    float totalCritPower = (critPower.GetValue() + strength.GetValue()) * .01f;
+    float critDamage = _damage * totalCritPower;
+    return Mathf.RoundToInt(critDamage);
   }
 }
