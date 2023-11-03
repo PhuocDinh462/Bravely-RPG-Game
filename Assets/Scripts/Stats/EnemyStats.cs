d8c98a9f4f52eb1486cf0a5e115e27c1218376b9
Phuoc Dinh <phuocdinh462@gmail.com>
2023-11-03 23:03:39 +0700

    Enemy modifiers

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -1,12 +1,54 @@
+using UnityEngine;
+
 public class EnemyStats : CharacterStats
 {
   private Enemy enemy;
 
+  [Header("Level details")]
+  [SerializeField] private int level = 1;
+
+  [Range(0f, 1f)]
+  [SerializeField] private float percentageModifier = .4f;
+
   protected override void Start()
   {
+    ApplyLevelModifiers();
+
     base.Start();
 
     enemy = GetComponent<Enemy>();
+
+  }
+
+  private void ApplyLevelModifiers()
+  {
+    modify(strength);
+    modify(agility);
+    modify(intelligence);
+    modify(vitality);
+
+    modify(damage);
+    modify(critChance);
+    modify(critPower);
+
+    modify(maxHealth);
+    modify(armor);
+    modify(evasion);
+    modify(magicResistance);
+
+    modify(fireDamage);
+    modify(iceDamage);
+    modify(lightningDamage);
+  }
+
+  private void modify(Stat _stat)
+  {
+    for (int i = 1; i < level; i++)
+    {
+      float modifier = _stat.GetValue() * percentageModifier;
+
+      _stat.AddModifier(Mathf.RoundToInt(modifier));
+    }
   }
 
   public override void TakeDamage(int _damage)
