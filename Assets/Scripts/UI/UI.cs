b25099b9d4e4ed13f05c94b36f9d11270d20ac15
Phuoc Dinh <phuocdinh462@gmail.com>
2023-11-08 00:27:04 +0700

    Switch UI with Keyboard

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,8 +1,12 @@
+using UnityEditor;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
   [SerializeField] private GameObject characterUI;
+  [SerializeField] private GameObject skillTreeUI;
+  [SerializeField] private GameObject craftUI;
+  [SerializeField] private GameObject optionUI;
 
   public UI_ItemTooltip itemTooltip;
   public UI_StatToolTip statToolTip;
@@ -10,13 +14,26 @@ public class UI : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    SwitchTo(null);
 
+    itemTooltip.gameObject.SetActive(false);
+    statToolTip.gameObject.SetActive(false);
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (Input.GetKeyDown(KeyCode.C))
+      SwitchWithKeyTo(characterUI);
 
+    if (Input.GetKeyDown(KeyCode.B))
+      SwitchWithKeyTo(craftUI);
+
+    if (Input.GetKeyDown(KeyCode.K))
+      SwitchWithKeyTo(skillTreeUI);
+
+    if (Input.GetKeyDown(KeyCode.O))
+      SwitchWithKeyTo(optionUI);
   }
 
   public void SwitchTo(GameObject _menu)
@@ -26,5 +43,16 @@ public class UI : MonoBehaviour
 
     if (_menu)
       _menu.SetActive(true);
+  }
+
+  public void SwitchWithKeyTo(GameObject _menu)
+  {
+    if (_menu && _menu.activeSelf)
+    {
+      _menu.SetActive(false);
+      return;
+    }
+
+    SwitchTo(_menu);
   }
 }
