1a19bf534d3f17422745dc4a3be13bdbe61d77ef
Phuoc Dinh <phuocdinh462@gmail.com>
2023-11-14 07:34:54 +0700

    Save Inventory Items

--- Assets/Scripts/Items and Inventory/Inventory.cs
+++ Assets/Scripts/Items and Inventory/Inventory.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour, ISaveManager
 {
   public static Inventory instance;
 
@@ -35,6 +36,9 @@ public class Inventory : MonoBehaviour
   public float flaskCooldown { get; private set; }
   private float armorCooldown;
 
+  [Header("Data base")]
+  public List<InventoryItem> loadedItems;
+
   private void Awake()
   {
     if (!instance)
@@ -64,6 +68,19 @@ public class Inventory : MonoBehaviour
 
   private void AddStartingItems()
   {
+    if (loadedItems.Count > 0)
+    {
+      foreach (InventoryItem item in loadedItems)
+      {
+        for (int i = 0; i < item.stackSize; i++)
+        {
+          AddItem(item.data);
+        }
+      }
+
+      return;
+    }
+
     for (int i = 0; i < startingItems.Count; i++)
     {
       if (startingItems[i] != null)
@@ -297,5 +314,47 @@ public class Inventory : MonoBehaviour
 
     Debug.Log("Armor on cooldown");
     return false;
+  }
+
+  public void LoadData(GameData _data)
+  {
+    foreach (KeyValuePair<string, int> pair in _data.inventory)
+    {
+      foreach (var item in GetItemDataBase())
+      {
+        if (item != null && item.itemId == pair.Key)
+        {
+          InventoryItem itemToLoad = new InventoryItem(item);
+          itemToLoad.stackSize = pair.Value;
+
+          loadedItems.Add(itemToLoad);
+        }
+      }
+    }
+  }
+
+  public void SaveData(ref GameData _data)
+  {
+    _data.inventory.Clear();
+
+    foreach (KeyValuePair<ItemData, InventoryItem> pair in inventoryDictionary)
+    {
+      _data.inventory.Add(pair.Key.itemId, pair.Value.stackSize);
+    }
+  }
+
+  private List<ItemData> GetItemDataBase()
+  {
+    List<ItemData> itemDataBase = new List<ItemData>();
+    string[] assetNames = AssetDatabase.FindAssets("", new[] { "Assets/Data/Equipment" });
+
+    foreach (string SOName in assetNames)
+    {
+      var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
+      var itemData = AssetDatabase.LoadAssetAtPath<ItemData>(SOpath);
+      itemDataBase.Add(itemData);
+    }
+
+    return itemDataBase;
   }
 }
